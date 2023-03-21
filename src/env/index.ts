3c1ec106c27b7ad2860290f57dd45aff2f33ea2a
Renan <renansilvaamorim1999@gmail.com>
2023-03-21 20:09:50 -0300

    chore: Production config

--- src/env/index.ts
+++ src/env/index.ts
@@ -10,6 +10,7 @@ if (process.env.NODE_ENV === 'test') {
 // Define um esquema de validação para as variáveis de ambiente esperadas
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
   PORT: z.coerce.number().default(3333)
 });
